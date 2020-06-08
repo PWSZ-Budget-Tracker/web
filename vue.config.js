5badc510ffd2b9fd115d76e8c28ec37920e529e6
kingasz <kingaszczepaniak@op.pl>
2020-06-08 20:13:41 +0200

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/web/dist/'
+    : '/'
+}
 };
